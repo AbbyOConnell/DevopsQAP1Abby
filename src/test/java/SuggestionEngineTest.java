be515a45c61f4722d943d45bcacaa22a417d8e74
AbbyOConnell <116213683+AbbyOConnell@users.noreply.github.com>
2023-10-12 18:25:39 -0230

    i like cats

---
+++ src/test/java/SuggestionEngineTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+import com.keyin.SuggestionEngine;
+import com.keyin.SuggestionsDatabase;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+
+
+class SuggestionEngineTest {
+
+    @org.junit.jupiter.api.Test
+    void loadDictionaryData() {
+        SuggestionEngine suggestionEngine = new SuggestionEngine();
+        try {
+            suggestionEngine.loadDictionaryData(Paths.get("src/main/resources/words.txt"));
+            Map<String, Integer> wordSuggestionDB = suggestionEngine.getWordSuggestionDB();
+            assertFalse(wordSuggestionDB.isEmpty());
+        } catch (Exception e) {
+            fail("Exception thrown while loading dictionary data: " + e.getMessage());
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void generateSuggestions() throws Exception {
+        SuggestionEngine suggestionEngine = new SuggestionEngine();
+        suggestionEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
+
+        String correctSpelling = "sleep";
+        String suggestionsForCorrectSpelling = suggestionEngine.generateSuggestions(correctSpelling);
+        assertEquals("", suggestionsForCorrectSpelling, "Suggestions should be empty for a correctly spelled word");
+
+        String misspelledWord = "sleel";
+        String suggestionsForMisspelledWord = suggestionEngine.generateSuggestions(misspelledWord);
+        Assertions.assertTrue(suggestionsForMisspelledWord.contains("sleep"), "Suggestions should contain 'sleep'");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setWordSuggestionDB() {
+        SuggestionEngine suggestionEngine = new SuggestionEngine();
+        try {
+            suggestionEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
+        } catch (Exception e) {
+            fail("Exception thrown while loading dictionary data: " + e.getMessage());
+        }
+
+        SuggestionsDatabase customDatabase = new SuggestionsDatabase();
+        customDatabase.addWord("example", 10);
+
+        suggestionEngine.setWordSuggestionDB(customDatabase);
+
+        assertEquals(customDatabase.getWordMap(), suggestionEngine.getWordSuggestionDB());
+    }
+
+}
